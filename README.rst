2917e9a7e6d84f047300333822ea9395856a1499
Daniel Oaks <daniel@danieloaks.net>
2015-06-22 18:39:57 +1000

    Add NickMask tests

--- README.rst
+++ README.rst
@@ -5,6 +5,13 @@ Tests for IRC parsers.
 **Note:** This is nowhere near done or ready for much use at all for anyone but me yet. Feel free to submit PRs.
 
 
+Testing
+-------
+The ``test.py`` script uses both the `ircreactor <https://github.com/mammon-ircd/ircreactor>`_ and `girc <https://github.com/DanielOaks/girc>`_ libraries as 'reference' implementations. Neither of these are on PyPi, and must be installed with ``python3 setup.py install`` on both of them.
+
+After that, simply run ``python3 test.py``. This will test everything we can test, and show the output.
+
+
 License
 -------
 Whatever belongs to me is released into the public domain, do whatever you want with it. Most of these originate from `grawity's tests <https://github.com/grawity/code/tree/master/lib/tests>`_ though, so they may be covered under that license.
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-# Testing split.yaml with ircreactor
+# Testing split.yaml with ircreactor and girc
 from ircreactor.envelope import RFC1459Message
+from girc.utils import NickMask, validate_hostname
 import yaml
+
 
 # SPLIT Tests
 print('Running split tests')
@@ -21,7 +23,7 @@ for test in data['tests']:
     # test atoms
     if 'tags' in atoms:
         if m.tags != atoms['tags']:
-            out += '   Decoding tags failed, was [{}]\n'.format(m.tags.lower())
+            out += '   Decoding tags failed, was [{}]\n'.format(m.tags)
             failed = True
 
     if 'verb' in atoms:
@@ -42,6 +44,49 @@ for test in data['tests']:
                 out += '   Decoding params failed\n'
                 failed = True
                 break
+
+    # fail message
+    if failed:
+        print(out)
+        failed_tests += 1
+    else:
+        passed_tests += 1
+
+print(' * Passed Tests:', passed_tests)
+print(' * Failed Tests:', failed_tests)
+print()
+
+
+# NICKMASK Tests
+print('Running nickmask tests')
+data = yaml.safe_load(open('nickmask.yaml').read())
+
+failed_tests = 0
+passed_tests = 0
+
+for test in data['tests']:
+    source = test['source']
+    atoms = test['atoms']
+    out = ' * Testing: [{}]\n'.format(source)
+
+    nm = NickMask(source)
+    failed = False
+
+    # test atoms
+    if 'nick' in atoms:
+        if nm.nick != atoms['nick']:
+            out += '   Decoding nick failed, was [{}]\n'.format(nm.nick)
+            failed = True
+
+    if 'user' in atoms:
+        if nm.user != atoms['user']:
+            out += '   Decoding user failed, was [{}]\n'.format(nm.user)
+            failed = True
+
+    if 'host' in atoms:
+        if nm.host != atoms['host']:
+            out += '   Decoding host failed, was [{}]\n'.format(nm.host)
+            failed = True
 
     # fail message
     if failed:
