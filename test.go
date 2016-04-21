e99665c957b487b9b88d4db8ec4ce987db96cec9
Daniel Oaks <daniel@danieloaks.net>
2016-04-22 09:22:29 +1000

    test.go: CC0 notice

--- test.go
+++ test.go
@@ -1,5 +1,15 @@
 package main
 
+// Written in 2016 by Daniel Oaks <daniel@danieloaks.net>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain
+// worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along
+// with this software. If not, see
+// <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 import (
 	"fmt"
 	"io/ioutil"
