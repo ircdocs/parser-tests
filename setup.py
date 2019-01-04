e95adfe02fd5699573a92e02731f0b6179715a10
linuxdaemon <linuxdaemon@snoonet.org>
2019-01-03 22:30:24 -0600

    Add additional metadata for PyPI

--- setup.py
+++ setup.py
@@ -24,9 +24,14 @@ with link_tests():
         name="irc-parser-tests",
         # Generate the version string from __version__ tuple
         version='.'.join(map(str, parser_tests.__version__)),
+        description="A library of tests for various IRC protocol parsers",
         url='https://github.com/ircdocs/parser-tests',
         author="linuxdaemon",
         author_email="linuxdaemonirc@gmail.com",
+        classifiers=[
+            'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        ],
+        keywords='irc tests parser',
         packages=['parser_tests'],
         install_requires=[
             'PyYaml',
